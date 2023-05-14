c29e5c55c1ab7dbc27ad6aba13a90fe3bf9ee4ce
Andrei Ioffe <andrei.ioffe@gmail.com>
2023-05-14 20:49:04 +0200

    evaluation done

--- main.py
+++ main.py
@@ -45,6 +45,94 @@ while i<= endColumnIndex :
     i += 1
 
 
+# Specify the file path
+file_path = 'Calculation rule.xlsx'
+
+# # Loop through each sheet and perform operations
+# for sheet_name, sheet_data in all_sheets.items():
+#     # Process the data from each sheet
+#     print(f"Sheet Name: {sheet_name}")
+#     print(sheet_data.head())  # Example: printing the first few rows
+#     print('-' * 30)
+
+
+
+
+my_dict = {}
+tempEvaluation = 0
+
+
+
+def evaluateScore(sheetIndex, file_path, resultSheet,startColumnIndex, ChelRowIndex):
+
+    startColumnIndex -=1
+
+
+
+    xl = pd.ExcelFile(file_path)
+
+    sheet_names = xl.sheet_names
+    sheet_name = sheet_names[sheetIndex]
+    df = pd.read_excel(file_path, sheet_name=sheet_name)
+    # print(f"Sheet Name: {sheet_name}")
+    # print(df.head())
+
+    totalPoints = 0
+    for index, row in df.iterrows():
+
+        if resultSheet.iloc[ChelRowIndex, startColumnIndex + int (row['Вопрос'])] == "Да" and row['Правильный ответ'] == "+":
+            totalPoints+=1
+        elif resultSheet.iloc[ChelRowIndex, startColumnIndex + int (row['Вопрос'])] == "Нет" and row['Правильный ответ'] == "-":
+            totalPoints+=1
+        elif resultSheet.iloc[ChelRowIndex, startColumnIndex + int(row['Вопрос'])] == "Не уверен" :
+            totalPoints += 0
+
+    my_dict = {}
+
+    # Add elements dynamically
+    my_dict[sheet_name] = totalPoints
+    return my_dict
+
+
+        # Access row data using column names or indices
+        # print(f"Row {index + 1}: {row['Вопрос']}, {row['Правильный ответ']})  # Example
+
+    # if resultSheet.
+    #
+    #     cell_value = df.iloc[RowIndex, i]
+    #     print(df.columns[i] + cell_value + "\n")
+
+class Chel:
+    def __init__(self, name, dictResults):
+        self.name = name
+        self.dictResults = dictResults
+
+    def __str__(self):
+        return f"Name: {self.name}, Results: {self.dictResults}"
+
+
+ChelList = []
+
+for index, row in df.iterrows():
+    resultDict = []
+    for sheetIndex in range(7):
+        resultDict.append(evaluateScore(sheetIndex, file_path, df, startColumnIndex, index))
+
+    nameSurname = df.loc[index, nameColumn]
+    print(nameSurname ,"\n")
+    print(resultDict)
+    print("-" * 30)
+
+    ChelList.append(Chel(nameSurname,resultDict))
+
+for i in ChelList:
+    print(i)
+
+
+# Access a cell value by column name and index
+
+
+
 
 
 
