5ac04d099b25b6aab21d177f6c5a6859c171bbe3
Andrei Ioffe <andrei.ioffe@gmail.com>
2023-05-14 13:24:46 +0200

    Working Questions to answer mapping

--- main.py
+++ main.py
@@ -1,1 +1,53 @@
-print("1")
+
+
+import pandas as pd
+
+
+# Read the CSV file into a DataFrame
+df = pd.read_csv('Test.csv')
+
+nameColumn = "Укажи здесь свои данные, чтобы мы могли отправить тебе результаты. [Имя и фамилия]"
+
+RowIndex = 0
+# Access a cell value by column name and index
+nameSurname = df.loc[RowIndex, nameColumn]
+
+# Print the cell value
+print(nameSurname)
+
+emailColumn = "Укажи здесь свои данные, чтобы мы могли отправить тебе результаты. [Email адрес]"
+
+email = df.loc[RowIndex, emailColumn]
+
+
+
+# Print the cell value
+print(email)
+
+firstQuestion = "Если кто-то захочет вас прогнать, вы почувствуете необходимость что-то предпринять в связи с этим?"
+lastQuestion = "Вы любили в детстве играть с оружием? "
+
+
+firstAnswer = df.loc[RowIndex, firstQuestion]
+
+
+# Print the cell value
+print(firstAnswer)
+
+startColumnIndex = df.columns.get_loc(firstQuestion)
+
+endColumnIndex = df.columns.get_loc(lastQuestion)
+
+i = startColumnIndex
+while i<= endColumnIndex :
+    cell_value = df.iloc[RowIndex, i]
+    print(df.columns[i] + cell_value + "\n")
+    i += 1
+
+
+
+
+
+
+
+
